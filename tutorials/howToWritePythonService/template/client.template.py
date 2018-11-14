3247f330e01c1736683f7805e5ef0ce4f75d5261
arturgontijo <arturgontijo@gmail.com>
2018-11-14 09:14:43 -0200

    [howToWriteService] New template ext

--- tutorials/howToWritePythonService/template/client.template.py
+++ tutorials/howToWritePythonService/template/client.template.py
@@ -8,6 +8,7 @@ sys.path.append("./service_spec")
 import __PYTHON_PB2__ as pb2
 import __PYTHON_PB2_GRPC__ as pb2_grpc
 
+
 # TEST_CODE
 def doSomething(channel):
     # Check the compiled proto file (.py) to get method names
@@ -15,11 +16,13 @@ def doSomething(channel):
     response = stub.doSomething(pb2.Input())
     return response
 
+
 def main():
     # Connect to the server
     with grpc.insecure_channel('localhost:__SERVICE_PORT__') as channel:
         # Call TEST_CODE
         doSomething(channel)
+
 
 if __name__ == '__main__':
     main()
